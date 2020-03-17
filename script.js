9f242cd36a1d0233910b84dc400ba4ef2909bc4a
radioaktive <radioaktive@protonmail.com>
2020-03-17 13:13:12 +0300

    typo

--- script.js
+++ script.js
@@ -189,7 +189,7 @@
 			init: function () {
 			var notifications = self.checkNotifications();
 			var phone = self.get_settings().customer;
-			var parameter ='{' + parameter + phone ': "text"}';
+			var parameter ='{' + parameter + phone + ': "text"}';
 			self.set_settings(parameter);
 			var settingsWidget = self.get_settings();
 			console.dir(settingsWidget);
